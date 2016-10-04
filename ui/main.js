d2c57b1bcf031b9c095694d58f7ad4c357616278
AkhilSiva91 <akhilsiva91@gmail.com>
2016-10-04 20:42:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById('maintext');
 element.innerHTML = 'New Text Content';
 var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = margimLeft + 'px';
-}
+
 img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
+    img.style.marginLeft = '100px';
 };
