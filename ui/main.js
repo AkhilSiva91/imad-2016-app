07d4297a5aee737503385428c3032d690c63fc91
AkhilSiva91 <akhilsiva91@gmail.com>
2016-10-05 14:04:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,10 @@
 console.log('Loaded!');
-
+//changing the content
 var element = document.getElementById('mtext');
 element.innerHTML = 'New Text Content';
+
+
+//making the picture move
 var img = document.getElementById('madi');
 
 img.onclick = function(){
