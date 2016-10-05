e68707aef9ce03a216e6f3633d7b81e767897ed9
AkhilSiva91 <akhilsiva91@gmail.com>
2016-10-05 14:22:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,12 @@ element.innerHTML = 'New Text Content';
 
 //making the picture move
 var img = document.getElementById('madi');
-
+var marginLeft =0;
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginleft + 'px';
+}
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+ var interval = setInterval(moveRight, 50);   
 };
+
